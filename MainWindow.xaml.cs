debce20124299fdcdf48465efd62737a390277f0
rjdresurreccion <rjdresurreccion@gmail.com>
2018-01-26 13:49:44 +0800

    SampleCommit
    1

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -18,6 +18,7 @@ namespace LibrarySystem
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// oppa pits
     public partial class MainWindow : Window
     {
         public MainWindow()
